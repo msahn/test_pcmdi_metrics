e7df354a7bb3e5e2f36e0ce71d2ed54e44abe7aa
Min-Seop Ahn <ahn6@llnl.gov>
2020-07-02 14:48:47 -0700

    add matplotlib

---
+++ matplotlib/plot_baseman.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Read data (nc file)
+
+# In[1]:
+
+
+import cdms2 as cdms
+
+var = 'pr'
+data = 'GPCP-2-3'
+period = '197901-201907'
+ver = 'v20200421'
+
+dir = '/p/user_pub/PCMDIobs/PCMDIobs2_clims/atmos/'+var+'/'+data+'/'
+nc = var+'_mon_'+data+'_BE_gn_'+period+'.'+ver+'.AC.nc'
+f = cdms.open(dir+nc)
+d = f[var]
+print(d.shape)
+
+
+# # Plot data
+
+# In[2]:
+
+
+from class_PlotBaseMap import PlotBaseMap
+
+PlotBaseMap(d[0])
+# PlotBaseMap(d[0], vmin='0', vmax='0.0003')
+
+
+# # land/sea masking with original grid
+
+# In[3]:
+
+
+from class_LandSeaMask import LandSeaMask
+
+mask = LandSeaMask(d)
+
+d_mask = mask.mask()
+d_land = mask.land()
+d_ocean = mask.ocean()
+
+d_mask.id = 'land_sea_mask'
+PlotBaseMap(d_mask)
+d_land.id = 'pr_land'
+PlotBaseMap(d_land[0])
+d_ocean.id = 'pr_ocean'
+PlotBaseMap(d_ocean[0])
+
+
+# # Write data (nc file)
+
+# In[4]:
+
+
+out = cdms.open('land_sea_mask.nc', 'w')
+out.write(d_mask)
+out.close()
+
+out = cdms.open('mask_land.nc', 'w')
+out.write(d_land)
+out.close()
+
+out = cdms.open('mask_ocean.nc', 'w')
+out.write(d_ocean)
+out.close()
+
+
+# In[5]:
+
+
+get_ipython().system('jupyter nbconvert --to script test_class.ipynb')
+
